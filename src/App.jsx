ac1c4f6cd18a30c5db9330ec91ca1eaeca8d3b5d
Cristopher Puma <capuma@puce.edu.ec>
2024-10-19 00:04:25 -0500

    Implementación de imagen fuente

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainPage from './Components/Main_page';
+import Start_page from './Components/Start_page';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/main" element={<Start_page />} />
       </Routes>
     </Router>
   );
-  
 }
 
 export default App;
