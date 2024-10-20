fd6f61a1b6e4d745a26edf2aa907488bd623868c
Cristopher Puma <capuma@puce.edu.ec>
2024-10-20 16:36:13 -0500

    Correción mediaquery-móvil

--- src/Components/Start_page.jsx
+++ src/Components/Start_page.jsx
@@ -3,8 +3,9 @@ import Styles_page from './Styles/Start_page.module.scss';
 const Start_page = () => {
     return (
         <header className={Styles_page.header}>
+            
             <div className={Styles_page.header__content}>
-                <h1 className={Styles_page.header__title}>Bienvenido</h1>
+            <h1 className={Styles_page.header__title}>Bienvenido</h1>
                 <p className={Styles_page.header__text}>Cuentanos tu experiencia con nuestros productos</p>
             </div>
         </header>
